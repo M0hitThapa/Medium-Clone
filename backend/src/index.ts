a272ccff03c75ec17bfc7d5e9c6dcf427e88ef5d
Mohit Thapa <thapamohit2058@gmail.com>
2025-03-17 23:03:22 +0545

    add middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 
 
 const app = new Hono<{
@@ -10,6 +10,22 @@ const app = new Hono<{
     JWT_SECRET: string,
   }
 }>()
+
+
+
+app.use('/api/v1/blog/*', async (c, next) => {
+const header = c.req.header("authorization") || "";
+
+const token = header.split("")[1]
+  const response = await verify(header, c.env.JWT_SECRET);
+  if (response.id) {
+    next()
+  } else {
+    c.status(403)
+    return c.json({error: "unauthorized"})
+  }
+  await next();
+})
 
 
 app.post('/api/v1/signup', async (c) => {
